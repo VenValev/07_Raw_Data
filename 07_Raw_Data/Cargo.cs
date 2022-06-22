630f228310fbef2515105926ce5fdfbf4548f4b5
Vencislav <vencislav67@hotmail.com>
2022-06-22 21:41:08 +0300

    7

--- 07_Raw_Data/Cargo.cs
+++ 07_Raw_Data/Cargo.cs
@@ -6,7 +6,10 @@ namespace _07_Raw_Data
 {
     internal class Cargo
     {
-        private string type;
+        private string vid;
         private int weight;
+
+        public string Vid { get; set; }
+        private int Weight { get; set; }
     }
 }
