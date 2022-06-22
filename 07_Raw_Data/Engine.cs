c2ee0dc41dcd50e32750c335bc116ce02e791b83
Vencislav <vencislav67@hotmail.com>
2022-06-22 21:40:03 +0300

    7

--- 07_Raw_Data/Engine.cs
+++ 07_Raw_Data/Engine.cs
@@ -8,5 +8,13 @@ namespace _07_Raw_Data
     {
         private int speed;
         private int power;
+        public Engine(int speed, int power)
+        {
+            Speed = speed;
+            Power = power; 
+        }
+
+        public int Speed { get; set; }
+        public int Power { get; set; }
     }
 }
