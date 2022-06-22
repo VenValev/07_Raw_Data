f7aa3f9f2941d52dc738eed2505f8207c61ad49c
Vencislav <vencislav67@hotmail.com>
2022-06-22 21:42:41 +0300

    7

--- 07_Raw_Data/Cargo.cs
+++ 07_Raw_Data/Cargo.cs
@@ -4,11 +4,16 @@ using System.Text;
 
 namespace _07_Raw_Data
 {
-    internal class Cargo
+    public class Cargo
     {
         private string vid;
         private int weight;
 
+        public Cargo(string vid, int weight)
+        {
+            Vid = vid;
+            Weight = weight;
+        }
         public string Vid { get; set; }
         private int Weight { get; set; }
     }
